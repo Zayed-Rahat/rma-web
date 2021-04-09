b5aea4694001ed5aefefabce823030dac271d82f
Ahammed Zayed Uddin Rahat <azurahat129@gmail.com>
2021-04-10 01:07:15 +0600

    commit

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Services() {
@@ -11,7 +12,7 @@ function Services() {
         </div>
         <div class="row our_services_footer_four_images">
             <div class="col-md-6 cc">
-                <img src="/ast/img/cc.png" class="img_cc img-fluid" alt=""/>
+              <Link> <img src="/ast/img/cc.png" class="img_cc img-fluid" alt=""/></Link> 
                 <div class="cc_wraper">
                     <h4 class="creat">Creative</h4>
                     <h2 class="consult">Counsultancy</h2>
@@ -19,13 +20,14 @@ function Services() {
             </div>
             <div class="col-md-3 pm_div">
                 <div class="pm_wrapper">
-                    <img src="/ast/img/pm.png" class="img_pm img_pm1 img-fluid" alt=""/>
+                   <Link> <img src="/ast/img/pm.png" class="img_pm img_pm1 img-fluid" alt=""/>
+                   </Link> 
                     <div class="pm">
                         <h5 class="pm_h pm_h1">Project <br/> Making</h5>
                     </div>
                 </div>
                 <div class="pm_wrapper dd">
-                    <img src="/ast/img/dd.png" class="img_pm img_dd img-fluid" alt=""/>
+                   <Link><img src="/ast/img/dd.png" class="img_pm img_dd img-fluid" alt=""/> </Link> 
                     <div class="pm">
                         <h5 class="pm_h pm_dd h5">DIGITAL</h5>
                         <h4 class="pm_h pm_dd h4">DESIGNING</h4>
@@ -33,7 +35,9 @@ function Services() {
                 </div>
             </div>
             <div class="col-md-3 rd-wrapper">
-                <img src="/ast/img/rd.png" class="img_rd img-fluid" alt=""/>
+              <Link>
+              <img src="/ast/img/rd.png" class="img_rd img-fluid" alt=""/>
+              </Link>  
                 <div class="rd">
                     <h5 class="rd_h5">Research &</h5>
                     <h4 class="rd_h4">Development</h4>
